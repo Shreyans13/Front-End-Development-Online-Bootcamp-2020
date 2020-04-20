8351b94f7403c3e6cd7c95ed97f53d39e1b9058d
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2020-04-20 23:07:03 +0530

    sass used

--- To Do List/style.js
+++ To Do List/style.js
@@ -1,53 +1,45 @@
-var modal = document.getElementById("modelContainer");
-var editModel = document.getElementById("editModelContainer");
-var span = document.querySelectorAll("span")
-var cancel = document.getElementById("CancelAdd");
-var cancelEdit = document.getElementById("CancelEdit");
+let modal = document.getElementById("modelContainer");
+let editModel = document.getElementById("editModelContainer");
+let span = document.querySelectorAll("span")
+let cancel = document.getElementById("CancelAdd");
+let cancelEdit = document.getElementById("CancelEdit");
 
 document.getElementById("buttonAdd").addEventListener("click", function() {
   modal.style.display = "block";
   randomColor()
 })
-span[0].onclick = function() {
+span[0].onclick = () => {
   	modal.style.display = "none";
 }
 document.getElementById('closeBox').addEventListener("click", function() {
   	editModel.style.display = "none";
 })
-cancel.onclick = function () {
+cancel.onclick = () => {
   	modal.style.display = "none";
-	// body...
 }
-cancelEdit.onclick = function () {
+cancelEdit.onclick = () =>{
   	editModel.style.display = "none";
-  	// body...
 }
   	
-var data = [];
-function getInput() {
-	var n = document.getElementById('name').value
-	var d = document.getElementById('description').value
-	if (n.length>0 && d.length>0) {
-		var dataToAdd = {
-			name: n,
-			desc: d,
+let data = [];
+let dataAdder = () =>{
+	let dataToAdd = {
+			name: document.getElementById('name').value,
+			desc: document.getElementById('description').value,
 			status: false
 		}
 		document.getElementById('name').value = ''
 		document.getElementById('description').value = ''
 		data.unshift(dataToAdd)
 		printData()
-	}
-	else {
-		alert('No input')
-		return;
-	}
 }
-
-function printData() {
+let getInput = () =>{
+	(document.getElementById('name').value.length>0 && document.getElementById('description').value.length>0) ? dataAdder() : alert('No Input or Incomplete Input')
+}
+let printData = () => {
 	randomColor()
 	document.getElementById('container').innerHTML = '';
-	for (var i = 0; i < data.length; i++) {
+	for (let i = 0; i < data.length; i++) {
 		if (!data[i].status){
 			document.getElementById('container').innerHTML += `
 				<div class="task" id=${i}>
@@ -80,42 +72,28 @@ function printData() {
 			`;
 		}
 	}
-
-
-		modal.style.display = "none";
-
-	// body...
+	modal.style.display = "none";
 }
 document.getElementById("confirmAdd").addEventListener("click", getInput);
 
-
-// function deleteData(index) {
 let deleteData = (index) => {
 	if (confirm("Are you sure you want to delete ?")) {
 		console.log(index)
-		// data[index].status = true;
-		// delete(data[index])
 		data.splice(index, 1);
-		// Array.prototype.map.call(document.querySelectorAll('.task'),(ob)=>{
-		// 	console.log("ob.id = "+ob.id)
-		// 	ob.className = (ob.id==index) ? "task none" : ob.className;
-		// })
 		printData()
 	}
 }
-var editIndex;
-// function editData(index) {
+let editIndex;
 let editData = (index) => {
 	console.log(index)
 	editIndex = index;
 	editModel.style.display = "block";
 }
 
-// function confirmEdit(index) {
 let confirmEdit = (index) => {
 	editModel.style.display = "none";
-	var nn = document.getElementById("newName").value
-	var dd = document.getElementById("newDescription").value
+	let nn = document.getElementById("newName").value
+	let dd = document.getElementById("newDescription").value
 	if (nn.length > 0) {
 		data[editIndex].name = nn;
 	}
@@ -127,44 +105,18 @@ let confirmEdit = (index) => {
 	printData()
 }
 
-// function randomColor() {
 let randomColor = () => {
-	var colorletters = 'ABCDEF0123456789';
-	var color = '#';
-	for (var i = 0; i < 6; i++) {
+	let colorletters = 'ABCDEF0123456789';
+	let color = '#';
+	for (let i = 0; i < 6; i++) {
 		color += colorletters[Math.floor(Math.random() * 16)];
 	}
 	document.getElementById('navbar').style.background = color
 }
 
-
 let clicked = (index) => {
 	console.log(index)
 	data[index].status = !data[index].status;
 	printData()
-	// let change = (i) => {
-	// 	data[i].status = data[i].status ? "false" : "true"
-	// 	return "content linethrough"
-	// }
-	// let change2 = (i) => {
-	// 	data[i].status = data[i].status ? "false" : "true"
-	// 	return "content"
-	// }
-	// let c1 = () => {
-	// 	index.className = "fas fa-check-square box"
-	// 	Array.prototype.map.call(document.querySelectorAll('.content'),(ob)=>{
-	// 		ob.className = (ob.id==index.id.split(' ')[1]) ? change(ob.id) : ob.className
-
-	// 	})
-	// }
-	
-	// let c2 = () => {
-	// 	index.className = "far fa-check-square box"
-	// 	Array.prototype.map.call(document.querySelectorAll('.content'),(ob)=>{
-	// 		ob.className = (ob.id==index.id.split(' ')[1]) ? change2(ob.id) : ob.className
-	// 	})
-	// }
-	// (index.className=="far fa-check-square box")?c1():c2();
-
 }
 
